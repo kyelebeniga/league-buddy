eddf4c6139d749caccba4f4788a8282f0be5a01d
kyelebeniga <61011686+kyelebeniga@users.noreply.github.com>
2023-05-29 12:20:22 +0800

    Made code more concise

--- js/app.js
+++ js/app.js
@@ -15,92 +15,55 @@ const imageContainer = document.getElementById("image-container");
 
 // Fetch the champion data
 fetch(championDataURL)
-.then(function(response) {
-  // Extract the JSON data from the response
-  response.json()
-  .then(function(championData) {
-    // Loop through the data object to get all champion names
-    for (const championName in championData.data) {
-      if (championData.data.hasOwnProperty(championName)) {
-        console.log("Champion name:", championName);
+  .then(response => response.json())
+  .then(championData => {
+    const championNames = Object.keys(championData.data).sort();
 
-        // Get the champion title
-        const championTitle = championData.data[championName].title;
-        console.log("Champion title:", championTitle);
+    championNames.forEach(championName => {
+      const champion = championData.data[championName];
+      const championTitle = champion.title;
+      const championImageURL = `${championImageBaseURL}${championName}_0.jpg`;
 
-        // Construct the URL for the champion image using the champion name
-        const championImageURL = `${championImageBaseURL}${championName}_0.jpg`;
-        console.log("Champion image URL:", championImageURL);
+      fetch(championImageURL)
+        .then(response => response.blob())
+        .then(imageData => {
+          const div = document.createElement("div");
+          div.classList.add("championCard");
 
-        const championId = championData.data[championName].id;
+          const img = document.createElement("img");
+          img.src = URL.createObjectURL(imageData);
 
-        // Fetch the image data
-        fetch(championImageURL)
-        .then(function(response) {
-          // Extract the image data from the response
-          response.blob()
-          .then(function(imageData) {
-            // Create a new div element to contain the image and title
-            const div = document.createElement("div");
-            div.classList.add('championCard');
+          const h2 = document.createElement("h2");
+          h2.textContent = championName.replace(/([A-Z])/g, ' $1').trim();
 
-            // Create an image element and set the source to the retrieved image data
-            const img = document.createElement("img");
-            img.src = URL.createObjectURL(imageData);
+          const p = document.createElement("p");
+          p.textContent = championTitle;
 
-            // Create a heading element for the champion name and set the text to the champion name
-            const h2 = document.createElement("h2");
-            h2.textContent = championName.replace(/([A-Z])/g, ' $1').trim(); //Creates a space when a champion's name is more than one word. e.g Aurelion Sol
+          div.appendChild(h2);
+          div.appendChild(p);
+          div.appendChild(img);
+          imageContainer.appendChild(div);
 
-            // Create a paragraph element for the champion title and set the text to the champion title
-            const p = document.createElement("p");
-            p.textContent = championTitle;
-
-            // Add the heading and paragraph elements to the div
-            div.appendChild(h2);
-            div.appendChild(p);
-
-            // Add the image element to the div
-            div.appendChild(img);
-
-            // Add the div to the image container
-            imageContainer.appendChild(div);
-
-            // Redirects to champion specific page when clicked
-            div.addEventListener('click', function(){
-              console.log(`You clicked ${championName}`);
-              localStorage.setItem('championId', championId);
-              // window.location.href = `/league-buddy/champPage.html`;
-              window.location.href = `/champPage.html`;
-            })
+          div.addEventListener('click', () => {
+            console.log(`You clicked ${championName}`);
+            localStorage.setItem('championId', champion.id);
+            window.location.href = '/champPage.html';
           });
         })
-        .catch(function(error) {
-          console.log(error);
-        });
-      }
-    }
-    // Add an event listener to the search input element to filter the displayed champions
-    const searchInput = document.getElementById("search-input");
-    searchInput.addEventListener("input", function() {
-        // Get the search query entered by the user
-        const searchQuery = this.value.toLowerCase();
-  
-        // Get all champion name elements
-        const championNameElements = imageContainer.querySelectorAll("h2");
-  
-        // Loop through the champion name elements and hide/show based on the search query
-        championNameElements.forEach(function(championNameElement) {
-          const championName = championNameElement.textContent.toLowerCase();
-          if (championName.includes(searchQuery)) {
-            championNameElement.parentNode.style.display = "block";
-          } else {
-            championNameElement.parentNode.style.display = "none";
-          }
-        });
-      });
+        .catch(error => console.log(error));
     });
   })
-  .catch(function(error) {
-    console.log(error);
+.catch(error => console.log(error));
+
+// Searches for a champion
+const searchInput = document.getElementById("search-input");
+searchInput.addEventListener("input", function() {
+  const searchQuery = this.value.toLowerCase();
+  const championNameElements = imageContainer.querySelectorAll("h2");
+
+  championNameElements.forEach(championNameElement => {
+    const championName = championNameElement.textContent.toLowerCase();
+    championNameElement.parentNode.style.display = championName.includes(searchQuery) ? "block" : "none";
   });
+});
+
