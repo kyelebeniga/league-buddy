8aa5c43cb5e997c8668c6d4b3257576e9198ba4e
kyelebeniga <61011686+kyelebeniga@users.noreply.github.com>
2023-05-30 15:50:25 +0800

    Created skin sidebar

--- js/skins.js
+++ js/skins.js
@@ -1,17 +1,36 @@
-// const championDataURL = `http://ddragon.leagueoflegends.com/cdn/13.9.1/data/en_US/champion/${championName}.json`;
-// const championImageBaseURL = "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/";
-
 fetch(championDataURL)
 .then(function(response){
     response.json()
     .then(function(data){
+        // Gets skin data from response.json and puts it into a map
         const skinURL = Object.values(data.data[championName].skins)
         .map(data => ({
             skinName: data.name,
             skinArt: `${championImageBaseURL}${championName}_${data.num}.jpg`
         }))
+        // Creates a div for each skin
+        let isFirstIteration = true;
         skinURL.forEach(skin => {
-            
+            const skinName = skin.skinName;
+            const skinSideBar = document.querySelector(".skin-sidebar");
+
+            const skinDiv = document.createElement("div");
+            skinDiv.classList.add("skin-element");
+
+            const p = document.createElement("p");
+            p.textContent = skinName;
+            // Renames the default skin to the champion's name
+            if(isFirstIteration){
+                p.textContent = championName;
+                isFirstIteration = false;
+            }
+
+            skinSideBar.appendChild(skinDiv);
+            skinDiv.appendChild(p);
+
+            skinDiv.addEventListener('click', () => {
+                console.log(skinName);
+            })
         })
     })
 })
